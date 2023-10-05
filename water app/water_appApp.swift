44fa2c70ac4932eeab05e347e4da1f0b300a03b0
somooo <somoooy@yahoo.com>
2023-10-05 12:43:59 +0300

    Initial Commit

---
+++ water app/water_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  water_appApp.swift
+//  water app
+//
+//  Created by bsamh on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct water_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
